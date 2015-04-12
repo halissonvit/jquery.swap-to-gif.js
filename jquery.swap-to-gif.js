887bc92304e0b023c15754dd677c8e2361c535f8
Halisson Vitor <halissonvit@gmail.com>
2015-04-11 23:39:38 -0300

    fixing error when there is no match

--- jquery.swap-to-gif.js
+++ jquery.swap-to-gif.js
@@ -3,15 +3,17 @@
      * Swaps an static image (png, jpg, jpeg, bmp, etc) to its animated version.
      */
     $.fn.swapToGif = function () {
-        var staticImg = this.attr('src');
-        var gifImg = this.data('gif-src') ? this.data('gif-src') : staticImg.substr(0, staticImg.lastIndexOf(".")) + ".gif";
+        this.each(function () {
+            var staticImg = $(this).attr('src');
+            var gifImg = $(this).data('gif-src') ? $(this).data('gif-src') : staticImg.substr(0, staticImg.lastIndexOf(".")) + ".gif";
 
-        this.hover(function () {
-            $(this).attr('src', gifImg);
+            $(this).hover(function () {
+                $(this).attr('src', gifImg);
 
-        }, function () {
-            // on mouse leave
-            $(this).attr('src', staticImg);
+            }, function () {
+                // on mouse leave
+                $(this).attr('src', staticImg);
+            });
         });
     };
 })(jQuery);
